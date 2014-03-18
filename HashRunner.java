4a27d6206006c05ec7c510474b94134949094d65
vagrant <mpgarate@gmail.com>
2014-03-18 09:25:43 -0400

    simple hash table working

--- HashRunner.java
+++ HashRunner.java
@@ -2,6 +2,12 @@
 class HashRunner{
   public static void main(String[] args){
     HashTable hash = new HashTable();
-    
+
+    String key = "Michael";
+    int value = 20;
+
+    hash.add(key, value);
+
+    System.out.println(hash.toString());
   }
 }
--- HashTable.java
+++ HashTable.java
@@ -1,3 +1,42 @@
-public class HashTable{
+public class HashTable {
 
+  private final int TABLE_SIZE = 128;
+  private int[] table = new int[TABLE_SIZE];
+
+  public HashTable(){
+    initializeTable();
+  }
+
+  public void add(String str, int value){
+    int key = stringToHashKey(str);
+    table[key] = value;
+  }
+
+  public String toString(){
+    StringBuilder sb = new StringBuilder();
+
+    for(int i = 0; i < table.length; i++){
+      if (table[i] != -999){
+        sb.append(i + " : ");
+        sb.append(table[i]);
+        sb.append("\n");
+      }
+    }
+
+    return sb.toString();
+  }
+
+  private int stringToHashKey(String string){
+    int key = string.hashCode();
+    key = Math.abs(key % TABLE_SIZE);
+    System.out.println("key is: " + key);
+    return key;
+  }
+
+  // set default values to -999
+  private void initializeTable(){
+    for (int i = 0; i < table.length; i++){
+      table[i] = -999;
+    }
+  }
 }
